a8b780e4778f18b9a42db6b3417f10f360e745f2
geraldwuhoo <gerald@geraldwu.com>
2022-11-08 02:17:07 -0800

    Register custom repo using symlink following to accomodate different compression schemes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 if ! (aur repo > /dev/null 2>&1); then
     printf "Aur repo not initialized, initializing /repo...\n"
-    repo-add /repo/custom.db.tar.gz
+    repo-add /repo/custom.db.tar.xz
 fi
 
 sudo pacman --noconfirm -Syu
-repoctl conf new /repo/custom.db.tar.gz
+repoctl conf new "$(readlink -f /repo/custom.db)"
 exec "$@"
